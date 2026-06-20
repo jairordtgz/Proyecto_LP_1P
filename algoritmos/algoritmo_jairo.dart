34e3596ca1b70019e1572f2bc43a8065ed22c857
jairordtgz <jairodri.a05@gmail.com>
2026-06-20 00:04:06 -0500

    mejora algoritmo de prueba Jairo

--- algoritmos/algoritmo_jairo.dart
+++ algoritmos/algoritmo_jairo.dart
@@ -1,3 +1,13 @@
+int sumar(int a, int b) {
+return a + b;
+}
+
+double calcularPromedio(double n1, double n2) {
+return (n1 + n2) / 2;
+}
+
+List notas = [7.5, 8.0, 9.5];
+List nombres = ["Jairo", "Dylan", "Ellen"];
 int edad = 25
 double precio = 9.99
 String nombre = "JairoRodriguez"
@@ -25,3 +35,8 @@ bool aprobado = nota >= 60.0
 bool reprobado = nota < 60.0
 bool exacto = nota == 87.5
 bool distinto = nivel != 0
+
+int resultado = sumar(10, 5);
+double promedio = calcularPromedio(80.0, 95.0);
+
+String mensaje = "Prueba completada";
